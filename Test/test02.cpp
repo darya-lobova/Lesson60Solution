26a73859b7eae004ccba3a590341a820b75a1876
st <st@A510PC3>
2026-05-04 17:49:52 +0300

    test03 solved

--- Test/test02.cpp
+++ Test/test02.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
-	int n, m;
-
-	cout << "Input size of matrix (n x m): ";
-	cin >> n >> m;
-
-	int** matrix = new int* [n];
-
-	for (int i = 0; i < n; i++)
-	{
-		*(matrix + i) = new int[m];
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cout << *(*(matrix + i) + j) << " ";
-		}cout << "\n";
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		delete[] *(matrix + i);
-	}
-
-	delete[] matrix;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int n, m;
+//
+//	cout << "Input size of matrix (n x m): ";
+//	cin >> n >> m;
+//
+//	int** matrix = new int* [n];
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		*(matrix + i) = new int[m];
+//	}
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (int j = 0; j < m; j++)
+//		{
+//			cout << *(*(matrix + i) + j) << " ";
+//		}cout << "\n";
+//	}
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		delete[] * (matrix + i);
+//	}
+//
+//	delete[] matrix;
+//
+//	return 0;
+//}
---
+++ Test/test03.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n, m;
+
+	cout << "Input size of matrix (n x m): ";
+	cin >> n >> m;
+
+	int** matrix = new int* [n];
+	int* sizes = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		sizes[i] = rand() % m + 2;
+		matrix[i] = new int[sizes[i]];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < sizes[i]; j++)
+		{
+			matrix[i][j] = rand() % 100;
+			cout << matrix[i][j]<< " ";
+		}cout << "\n";
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] matrix[i];
+	}
+
+	delete[] matrix;
+
+	return 0;
+}
